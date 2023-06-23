176d5cc00cd0f2ad5fd383f45f7ff4f81bb39780
andrey1510 <andrey-1510@outlook.com>
2023-06-23 21:59:35 +0300

    new solutions added

---
+++ leetcode/easy/employees-earning-more-than-their-managers.sql
@@ -0,0 +1,21 @@
+#https://leetcode.com/problems/employees-earning-more-than-their-managers/
+
+
+#SQL schema 
+Create table If Not Exists Employee (id int, name varchar(255), salary int, managerId int);
+Truncate table Employee;
+insert into Employee (id, name, salary, managerId) values ('1', 'Joe', '70000', '3');
+insert into Employee (id, name, salary, managerId) values ('2', 'Henry', '80000', '4');
+insert into Employee (id, name, salary, managerId) values ('3', 'Sam', '60000', null);
+insert into Employee (id, name, salary, managerId) values ('4', 'Max', '90000', null);
+
+
+#MySQL Solution
+SELECT
+	e.NAME AS Employee 
+FROM
+	Employee AS e,
+	Employee AS m 
+WHERE
+	e.managerId = m.id 
+	AND e.salary > m.salary;
