53ff63e00c83e7aabe48e91b03a5751f1e190339
andrey1510 <andrey-1510@outlook.com>
2023-07-08 23:28:58 +0300

    New solutions added

---
+++ leetcode/easy/customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,23 @@
+#https://leetcode.com/problems/customer-placing-the-largest-number-of-orders/
+
+
+#MySQL schema 
+Create table If Not Exists orders (order_number int, customer_number int);
+Truncate table orders;
+insert into orders (order_number, customer_number) values ('1', '1');
+insert into orders (order_number, customer_number) values ('2', '2');
+insert into orders (order_number, customer_number) values ('3', '3');
+insert into orders (order_number, customer_number) values ('4', '3');
+
+
+#MySQL Solution
+SELECT 
+	customer_number
+FROM 
+	orders
+GROUP BY 
+	customer_number
+ORDER BY 
+	COUNT(order_number) DESC 
+LIMIT 
+	1;
